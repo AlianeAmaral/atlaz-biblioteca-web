ea60da419b7c3c6e43ec18d537e43d9882f188b7
Aliane <alianeamaral.tec@gmail.com>
2026-02-16 15:23:14 -0300

    feat(infrastructure): implementa classe BookService com getAllBooks

---
+++ src/infrastructure/services/BookService.ts
@@ -0,0 +1,18 @@
+import type { Book } from "../../domain/models/Book";
+
+export class BookService {
+
+  private readonly baseUrl = 'http://localhost:8081/books';
+
+  // busca todos os livros do back-end, retorna um promise que faz um array de objetos do tipo "Book"
+  async getAllBooks(): Promise<Book[]> {
+
+    const response = await fetch(this.baseUrl);
+
+    if (!response.ok) {
+      throw new Error(`Erro ao buscar livros: ${response.statusText}`);
+    }
+    return await response.json();
+  }
+  // aqui entram os métodos para criar, atualizar e deletar livros, seguindo a mesma estrutura do método getAllBooks
+}
