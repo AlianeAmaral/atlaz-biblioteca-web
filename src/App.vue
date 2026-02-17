430de5e1e53f822396079a61c5d205b0eb47484a
Aliane <alianeamaral.tec@gmail.com>
2026-02-17 00:10:32 -0300

    config: configura env.d.ts para leitura de arquivos .vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,12 @@
+<script setup lang="ts">
+  import Menu from './presentation/components/Menu.vue'
+</script>
 
+<template>
+  <div class="flex min-h-screen bg-slate-50">
+    <Menu />
+    <main class="flex-1 p-8 ml-64">
+      <router-view/>
+    </main>
+  </div>
+</template>
---
+++ src/presentation/components/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <aside>
+
+    <div>
+      <div>
+        <img src="/img_logo_global_transparent.png" class="w-24" alt="Atlaz Logo" />
+      </div>
+
+      <nav>
+        <router-link to="/" class="nav-link">
+          Página Inicial
+        </router-link>
+
+        <router-link to="/students" class="nav-link">
+          Alunos
+        </router-link>
+
+        <router-link to="/books" class="nav-link">
+          Livros
+        </router-link>
+
+        <router-link to="/loans" class="nav-link">
+          Empréstimos
+        </router-link>
+
+        <router-link to="/reports" class="nav-link">
+          Relatórios
+        </router-link>
+      </nav>
+    </div>
+  </aside>
+
+</template>
